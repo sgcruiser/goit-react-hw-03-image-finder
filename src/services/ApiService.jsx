076382bc381cea9540b00ebd9715e3d5c4521e4e
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-04-18 21:07:14 +0300

    Update ApiService.jsx

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -2,19 +2,19 @@ import axios from 'axios';
 
 const apiKey = '20348581-4182e159a5fcf959667e3ff42';
 
-// const instanse = axios.create();
+const instanse = axios.create();
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+instanse.defaults.baseURL = 'https://pixabay.com/api/';
 
-axios.defaults.params = {
+instanse.defaults.params = {
   key: apiKey,
   orientation: 'horizontal',
   image_type: 'photo',
   per_page: 12,
 };
 
-const ApiService = async ({ searchQuery, pageNumber }) => {
-  return await axios
+const ApiService = async ({ searchQuery = '', pageNumber = 1 }) => {
+  return await instanse
     .get('', { params: { q: searchQuery, page: pageNumber } })
     .then(({ data }) => data);
 };
